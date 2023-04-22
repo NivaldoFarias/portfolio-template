9b5834a67b54e608d8acfad02e6d5e6a2258851f
Nivaldo Farias <badivia.f@gmail.com>
2023-04-22 17:06:31 -0300

    initialize repo; add integrations and custom packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import vercel from "@astrojs/vercel/serverless";
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx(), image(), prefetch()],
+  output: "server",
+  adapter: vercel()
+});
