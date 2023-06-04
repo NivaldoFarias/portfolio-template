398787294ed2d4b55e796a59648cc19ea7e6f115
Nivaldo Farias <badivia.f@gmail.com>
2023-06-04 16:50:43 -0300

    feat: sort required project packages

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+/** @type { import("eslint").Linter.FlatConfig[] } */
+export default [
+	{
+		root: true,
+		parser: "astro-eslint-parser",
+		parserOptions: {
+			project: "tsconfig.json",
+			extraFileExtensions: [".astro"],
+		},
+		extends: [
+			"eslint:recommended",
+			"plugin:astro/jsx-a11y-strict",
+			"plugin:@typescript-eslint/recommended",
+		],
+		overrides: [
+			{
+				files: ["*.astro"],
+				parser: "astro-eslint-parser",
+				parserOptions: {
+					parser: "@typescript-eslint/parser",
+					extraFileExtensions: [".astro"],
+				},
+			},
+		],
+	},
+];
