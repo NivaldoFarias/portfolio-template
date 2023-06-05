0e74f854dcd53a359142345197059f29e4cc9071
Nivaldo Farias <badivia.f@gmail.com>
2023-06-05 15:33:49 -0300

    apply global styles; add `.mdx` files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,20 @@ import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import vercel from "@astrojs/vercel/static";
 
+// Markdown Plugins
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+
 export default defineConfig({
 	output: "static",
 	adapter: vercel({ analytics: true }),
 	integrations: [tailwind(), mdx(), image(), prefetch()],
+	markdown: {
+		shikiConfig: {
+			theme: "rose-pine-moon",
+			wrap: true,
+		},
+		remarkPlugins: [remarkToc],
+		rehypePlugins: [rehypeAccessibleEmojis],
+	},
 });
